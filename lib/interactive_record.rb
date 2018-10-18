099512d939751bf6d97c70d4eb120d6269554b20
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-18 16:42:16 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,6 +2,8 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  column_names.each { |name| attr_accessor name.to_sym }
+  
   def self.table_name
     to_s.downcase.pluralize
   end
