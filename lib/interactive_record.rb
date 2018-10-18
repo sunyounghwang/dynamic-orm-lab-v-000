c884d161993845b4f074377f60f2ef3308b8fbb9
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-18 18:08:33 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,7 +20,7 @@ class InteractiveRecord
 
   def self.find_by(attribute: value)
     sql = "SELECT * FROM #{table_name} WHERE #{:attribute} = ?"
-    DB[:conn].execute(sql, attribute)
+    DB[:conn].execute(sql, value)
   end
 
   def initialize(attributes={})
